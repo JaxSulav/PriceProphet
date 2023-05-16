450e282cf934fb1ec89e9c50c3ccdb4c0e7f02be
Sulav Shrestha <stha.sulabh@gmail.com>
2023-05-16 01:03:42 -0400

    [Handle errors]
    - Attribute error in one field won't skip other fields

--- DataScraping/ppScraper/ppScraper/settings.py
+++ DataScraping/ppScraper/ppScraper/settings.py
@@ -11,7 +11,6 @@ BOT_NAME = 'ppScraper'
 
 SPIDER_MODULES = ['ppScraper.spiders']
 NEWSPIDER_MODULE = 'ppScraper.spiders'
-USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 12_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
 
 
 
@@ -88,3 +87,15 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+user_agent_list = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+    'Mozilla/5.0 (iPhone; CPU iPhone OS 14_4_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1',
+    'Mozilla/4.0 (compatible; MSIE 9.0; Windows NT 6.1)',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36 Edg/87.0.664.75',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363',
+]
+
+## Set Fallback User-Agent
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.18363'
+
--- DataScraping/ppScraper/ppScraper/spiders/amazonSpider.py
+++ DataScraping/ppScraper/ppScraper/spiders/amazonSpider.py
@@ -10,30 +10,52 @@ class AmazonSpider(scrapy.Spider):
     def start_requests(self):
         search_items = ['mouse']
         for item in search_items:
-            search_url = f'https://www.amazon.ca/s?k={item}&page=1'
-            yield scrapy.Request(url=search_url, callback=self.parse, meta={'item': item, 'page': 1})
+            search_url = f'https://www.amazon.ca/s?k={item}'
+            yield scrapy.Request(url=search_url, callback=self.parse)
     
     def parse(self, response):
         products = response.css('.s-result-item')
 
-        for product in products:
+        for cnt, product in enumerate(products):
             product_link = product.css('h2 a::attr(href)').get()
             if product_link is not None:
                 yield response.follow(product_link, self.parse_product)
-                break
 
-        # next_page = response.css('.s-pagination-container a::attr(href)').get()
-        # if next_page:
-        #     yield response.follow(next_page, self.parse)
+        next_page = response.css('.s-pagination-container a::attr(href)').get()
+        if next_page:
+            yield response.follow(next_page, self.parse)
 
     def parse_product(self, response):
-        title = response.css('#productTitle::text').get().strip()
-        price = response.css('span.a-offscreen::text').get().strip()
-        brand = response.css('span.po-break-word::text').get().strip()
-        category = response.css('#nav-subnav').attrib['data-category']
-        category_display = response.css('span.nav-a-content::text').get()
-        description = response.css('#feature-bullets ul li ::text').getall()
-        description = ''.join([x for x in description])
+        try:
+            title = response.css('#productTitle::text').get().strip()
+        except AttributeError:
+            title = ""
+
+        try:
+            price = response.css('span.a-offscreen::text').get().strip()
+        except AttributeError:
+            price = "" 
+
+        try:
+            brand = response.css('span.po-break-word::text').get().strip()
+        except AttributeError:
+            brand = ""
+
+        try:
+            category = response.css('#nav-subnav').attrib['data-category']
+        except AttributeError:
+            category = ""
+        
+        try:
+            category_display = response.css('span.nav-a-content::text').get().replace('\n', '')
+        except AttributeError:
+            category_display = ""
+
+        try:
+            description = response.css('#feature-bullets ul li ::text').getall()
+            description = ''.join([x for x in description])
+        except AttributeError:
+            description = ""
 
         yield {
             'title': title,
